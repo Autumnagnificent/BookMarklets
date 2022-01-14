cfea2f204390ba10a782918cc132ddfdb412270b
Austin <70984570+SpunkyTheHedgeHog@users.noreply.github.com>
2022-01-14 09:13:46 -0800

    Contuinus Updates

--- Graphic Design is My Passion.js
+++ Graphic Design is My Passion.js
@@ -1,35 +1,42 @@
 javascript: (() => {
 
-    gd();
+	start(1000);
+	async function start(delay)
+	{
+		var id = window.setInterval(gd, delay);
+	}
 
-    window.addEventListener("scroll", function (event) {
-        gd();
-    });
+	async function gd() {
+		const allElements = document.querySelectorAll('*');
+		var images = document.images; 
 
-    function gd() {
-        const allElements = document.querySelectorAll('*');
+		for (let element of allElements) {
+			element.style.fontFamily = 'Comic Sans MS';
+			element.style.fontStyle = Math.random() > .75 ? 'bold' : 'normal';
+			element.style.fontSize = '' + (Math.random() + 1.25) + 'vw';
 
-        for (let element of allElements) {
+			var h = Math.random() * 360;
+			element.style.color = 'hsl(' + h + ' , 100%, 50%)';
+			h = Math.random() * 360;
+			element.style.background = 'hsl(' + h + ' , 100%, 50%)';
 
-            element.style.fontFamily = 'Comic Sans MS';
-            element.style.fontSize = '' + (Math.random() + 1.1) + 'vw';
+			var max = .35;
+			var r = (Math.random() * max * 2) - max;
+			element.style.transform = 'rotate(' + r + 'deg)';
 
-            var h = Math.random() * 360;
-            element.style.color = 'hsl(' + h + ' , 100%, 50%)';
-            h = Math.random() * 360;
-            element.style.background = 'hsl(' + h + ' , 100%, 50%)';
+			if (Math.random() < .1) {
+				h = Math.random() * 360;
+				element.style.textShadow = '1px 4px hsl(' + h + ' , 100%, 50%)';   
+			}
+			else {
+				element.style.textShadow = '';
+			}
+		}
+	}
 
-            var max = .45;
-            var r = (Math.random() * max * 2) - max;
-            element.style.transform = 'rotate(' + r + 'deg)';
-
-            if (Math.random() < .1) {
-                h = Math.random() * 360;
-                element.style.textShadow = '1px 4px hsl(' + h + ' , 100%, 50%)';   
-            }
-            else {
-                element.style.textShadow = '';
-            }
-        }
-    }
+	function mdelay(n) {
+		return new Promise(function (resolve) {
+			setTimeout(resolve, n * 1000);
+		});
+	}
 })();
