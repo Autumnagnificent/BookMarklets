916da8dd015987766729c9e49ae50a1999c312f4
Austin <70984570+SpunkyTheHedgeHog@users.noreply.github.com>
2022-01-17 11:26:25 -0800

    Bookmark

--- Graphic Design is My Passion.js
+++ Graphic Design is My Passion.js
@@ -1,8 +1,10 @@
+<a href="
 javascript: (() => {
 
 	var GDCURSOR;
+	var DoIt = true;
 
-	start(1000);
+	start(25);
 	async function start(delay)
 	{
 		document.addEventListener('mousemove', MoveCursor, event);
@@ -10,57 +12,60 @@ javascript: (() => {
 		GDCURSOR = document.createElement('img');
 		GDCURSOR.style.width = '48px';
 		GDCURSOR.style.height = '48px';
-		GDCURSOR.style.position = "fixed";
-		GDCURSOR.src = 'https://www.clipartmax.com/png/small/132-1322583_overlay-pixel-glitch-tumblr-80s-90s-8bit-computer-aesth-windows-95.png';
-		document.body.appendChild(GDCURSOR);
+		GDCURSOR.style.position = 'absolute';
+GDCURSOR.src = 'https://www.clipartmax.com/png/small/132-1322583_overlay-pixel-glitch-tumblr-80s-90s-8bit-computer-aesth-windows-95.png';
+document.body.appendChild(GDCURSOR);
 
-		var id = window.setInterval(gd, delay);
+var id = window.setInterval(gd, delay);
 	}
 
-	function MoveCursor(event)
-	{
-		var x = event.clientX;
-		var y = event.clientY;
-		console.log("MOUSE MOVED");
-		
-		GDCURSOR.style.left = x - 24 + 'px';
-		GDCURSOR.style.top = y - 24 + 'px';
-	}
+function MoveCursor(event) {
+	var x = event.clientX;
+	var y = event.clientY;
+	console.log('MOUSE MOVED');
 
-	async function gd() {
-		const allElements = document.querySelectorAll('*'); 
+	GDCURSOR.style.marginLeft = x - 24 + 'px';
+	GDCURSOR.style.marginTop = y - 24 + 'px';
+}
 
-		for (let element of allElements) {
-			element.style.fontFamily = 'Comic Sans MS';
-			element.style.fontStyle = Math.random() > .75 ? 'bold' : 'normal';
-			element.style.fontSize = '' + (Math.random() + 1.25) + 'vw';
+async function gd() {
 
-			var h = Math.random() * 360;
-			element.style.color = 'hsl(' + h + ' , 100%, 50%)';
+	if (!DoIt) return;
+	DoIt = false;
+
+	const allElements = document.querySelectorAll('*');
+
+	for (let element of allElements) {
+		element.style.fontFamily = 'Comic Sans MS';
+		element.style.fontStyle = Math.random() > .75 ? 'bold' : 'normal';
+		element.style.fontSize = '' + (Math.random() + 1.25) + 'vw';
+
+		var h = Math.random() * 360;
+		element.style.color = 'hsl(' + h + ' , 100%, 50%)';
+		h = Math.random() * 360;
+		element.style.background = 'hsl(' + h + ' , 100%, 50%)';
+
+		var max = .35;
+		var r = (Math.random() * max * 2) - max;
+		element.style.transform = 'rotate(' + r + 'deg)';
+
+		if (Math.random() < .1) {
 			h = Math.random() * 360;
-			element.style.background = 'hsl(' + h + ' , 100%, 50%)';
-
-			var max = .35;
-			var r = (Math.random() * max * 2) - max;
-			element.style.transform = 'rotate(' + r + 'deg)';
-
-			if (Math.random() < .1) {
-				h = Math.random() * 360;
-				element.style.textShadow = '1px 4px hsl(' + h + ' , 100%, 50%)';   
-			}
-			else {
-				element.style.textShadow = '';
-			}
-
-			element.style.cursor = 'none';
-
-			await mdelay((1 / allElements.length) / 2)
+			element.style.textShadow = '1px 4px hsl(' + h + ' , 100%, 50%)';
+		}
+		else {
+			element.style.textShadow = '';
 		}
 	}
+	await mdelay(1);
 
-	function mdelay(n) {
-		return new Promise(function (resolve) {
-			setTimeout(resolve, n * 1000);
-		});
-	}
-})();
+	DoIt = true;
+}
+
+function mdelay(n) {
+	return new Promise(function (resolve) {
+		setTimeout(resolve, n * 1000);
+	});
+}
+}) ();
+"> Graphic Design is my Passion! </a>
