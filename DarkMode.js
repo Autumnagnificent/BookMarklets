e12f66732ff597cbaa783b3a22cba5a13991e460
Austin <70984570+SpunkyTheHedgeHog@users.noreply.github.com>
2022-01-19 08:01:35 -0800

    aaa

--- DarkMode.js
+++ DarkMode.js
@@ -10,19 +10,18 @@ javascript: (() => {
         const allElements = document.getElementsByTagName("*");
 
         for (let element of allElements) {
+
+            element.style.userSelect = 'text';
+
             if (element.textContent != "") {
                 element.style.color = 'hsl(0, 0%, 85%)';
                 element.style.background = 'hsl(0, 0%, 5%)';
 
-                element.style.borderColor = 'hsl(0, 0%, 8%)';
-                element.style.borderRadius = '8px';
-                element.style.borderWidth = '1px';
-                element.style.borderStyle = 'solid';
+                element.style.boxShadow = '0px 0px 128px -148px white';
 
-                element.style.userSelect = 'text';
             }
             else {
-                element.style.color = 'hsl(0, 0%, 85%)';
+                element.style.color = 'hsl(0, 0%, 85.5%)';
                 element.style.background = 'hsl(0, 0%, 3.5%)';
             }
         }
