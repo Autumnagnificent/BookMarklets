42fa6e6f9293c60699dddd8206ea17141897b007
Austin <70984570+SpunkyTheHedgeHog@users.noreply.github.com>
2022-01-19 06:50:18 -0800

    Changes to Darkmode

--- DarkMode.js
+++ DarkMode.js
@@ -10,15 +10,21 @@ javascript: (() => {
         const allElements = document.getElementsByTagName("*");
 
         for (let element of allElements) {
-            element.style.color = 'hsl(0, 0%, 85%)';
-            element.style.background = 'hsl(0, 0%, 5%)';
+            if (element.textContent != "") {
+                element.style.color = 'hsl(0, 0%, 85%)';
+                element.style.background = 'hsl(0, 0%, 5%)';
 
-            element.style.borderColor = 'hsl(0, 0%, 8%)';
-            element.style.borderRadius = '8px';
-            element.style.borderWidth = '1px';
-            element.style.borderStyle = 'solid';
+                element.style.borderColor = 'hsl(0, 0%, 8%)';
+                element.style.borderRadius = '8px';
+                element.style.borderWidth = '1px';
+                element.style.borderStyle = 'solid';
 
-            element.style.userSelect = 'text';
+                element.style.userSelect = 'text';
+            }
+            else {
+                element.style.color = 'hsl(0, 0%, 85%)';
+                element.style.background = 'hsl(0, 0%, 3.5%)';
+            }
         }
     }
 })();
